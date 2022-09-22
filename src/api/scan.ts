4a6abe5947185825a36ed87e23b42db86984c1c0
zhusemin <semin@126.com>
2022-09-22 17:23:42 +0800

    zsm

--- src/api/scan.ts
+++ src/api/scan.ts
@@ -1,8 +1,12 @@
-import request from '../../src/utils/request'
-export async function waybillNo(params:any) {
+import request from "../../src/utils/request";
+export async function waybillNo(params: any) {
   return request({
-    url: '/api/site/reach/waybillNo',
-    method: 'post',
-    data: params
-  })
+    url: "/api/site/reach/waybillNo",
+    method: "post",
+    data: params,
+    headers:{ //post需求加reqType：json就是application/json
+      reqType: 'json',
+      // module: 'encrypt', //跨模块
+    }
+  });
 }
